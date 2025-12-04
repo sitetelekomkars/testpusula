6d046cd281049b5deac36c499cc93d890d28f877
dogus <karssitetelekom@gmail.com>
2025-12-04 21:28:27 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const BAKIM_MODU = false;
 // Apps Script URL'si
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzX0ILqiGzOFk18PyT7dZRGESEjoF9JsQIHdLXKuUIAkzRe5CdIjI1aoxPr_pO4c_mmzw/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycby3kd04k2u9XdVDD1-vdbQQAsHNW6WLIn8bNYxTlVCL3U1a0WqZo6oPp9zfBWIpwJEinQ/exec";
 let jokers = { call: 1, half: 1, double: 1 };
 let doubleChanceUsed = false;
 let firstAnswerIndex = -1;
